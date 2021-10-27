7293b3f4b0a718f9f4197ee3792a2aa7f564334e
AlexandradePaula <alexandra.de.paula@hotmail.com>
2021-10-27 15:29:01 -0300

    Commit inicial

---
+++ app/src/main/java/com/example/sorteio/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.sorteio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun sortearNumero(view: View){
+       var texto = findViewById(R.id.textoSorteio) as TextView
+       var numeroSorteado = Random().nextInt(11)
+      texto.setText("Número sorteado é: $numeroSorteado")
+
+    }
+}
